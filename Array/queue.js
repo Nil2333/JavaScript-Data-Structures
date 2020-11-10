acaa368ae71468c68bd86668011cda74a39c926e
fubaozhu <fubaozhu@vackbot.com>
2020-11-10 15:56:05 +0800

    # add stack & queue

--- Array/queue.js
+++ Array/queue.js
@@ -3,3 +3,13 @@
  * 只用 push 和 shift 完成增删的“数组”
  * 先进先出
  */
+const queue = []
+//进队列
+queue.push(1)
+queue.push(2)
+queue.push(3)
+//出队列
+while(queue.length){
+    console.log(queue[0])
+    queue.shift()
+}
--- Array/stack.js
+++ Array/stack.js
@@ -3,3 +3,13 @@
  * 只用 pop 和 push 完成增删的“数组”
  * 后进先出
  */
+const stack = []
+//入栈
+stack.push(1)
+stack.push(2)
+stack.push(3)
+//出栈
+while (stack.length){
+    console.log(stack[stack.length-1])
+    stack.pop()
+}
